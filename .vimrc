4ce70198c4e4abbde6798977eede47172ecda084
Ritvik Saraf <13ritvik@gmail.com>
2018-03-17 23:02:57 +0530

    added vimrc and pathogen for plugins

---
+++ .vimrc
@@ -0,0 +1,12 @@
+
+runtime bundle/vim-pathogen/autoload/pathogen.vim
+
+"good color scheme, less dark colors
+:color desert
+
+"plugin management
+execute pathogen#infect()
+
+syntax on
+"https://goo.gl/9dwKKj
+filetype plugin indent on
--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #symlinks
 ln -siv "$DOTFILES_DIR/.bash_aliases" ~
-ln -siv "$DOTFILES_DIR/.bashrc" ~ 
+ln -siv "$DOTFILES_DIR/.bashrc" ~
+ln -siv "$DOTFILES_DIR/.vimrc" ~
+ln -siv "$DOTFILES_DIR/.vim" ~
