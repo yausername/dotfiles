aca9ba043976cc4f8c14eed337fe4659f20c9a1e
Ritvik Saraf <13ritvik@gmail.com>
2018-03-17 19:06:41 +0530

    added bashrc, alias, install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#get dotfiles directory
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#symlinks
+ln -siv "$DOTFILES_DIR/.bash_aliases" ~
+ln -siv "$DOTFILES_DIR/.bashrc" ~ 
